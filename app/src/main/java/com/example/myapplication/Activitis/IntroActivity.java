9e3354534c7d58891b3b0f2b5bc27a09877d3f24
Gustavoxky <gustavoxky@gmail.com>
2023-07-03 23:05:34 -0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/Activitis/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.Activitis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.myapplication.R;
+
+public class IntroActivity extends AppCompatActivity {
+private ImageView goBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        initView();
+        setVariable();
+    }
+
+    private void initView() {
+        goBtn = findViewById(R.id.goBtn);
+    }
+    private void setVariable() {
+        goBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this,LoginActivity.class)));
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Activitis/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication.Activitis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+private EditText userEdt,passEdt;
+private Button login_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initView();
+        setVariable();
+    }
+    private void initView() {
+        userEdt = findViewById(R.id.editTextTextUsername);
+        passEdt = findViewById(R.id.editTextTextPassword);
+        login_btn = findViewById(R.id.login_btn);
+    }
+    private void setVariable() {
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please fill the login form", Toast.LENGTH_SHORT).show();
+                } else if (userEdt.getText().toString().equals("admin") && passEdt.getText().toString().equals("admin")) {
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                }
+            }
+        });
+    }
+}
